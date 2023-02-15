b24be0722433c698db709953dae217290fd59227
Rishav Nandi <rishavnandi2@gmail.com>
2023-02-15 20:37:39 +0530

    Modifying tech stack icons

--- src/data/technologies.tsx
+++ src/data/technologies.tsx
@@ -1,50 +1,45 @@
 import {
-  Ansible,
-  Terraform,
-  Docker,
-  Shell,
-  Python,
-  Java,
-  Cplus,
-  Git,
-  Github,
-} from "react-icons";
+  SiAnsible,
+  SiTerraform,
+  SiDocker,
+  SiGnubash,
+  SiPython,
+  SiCplusplus,
+  SiGit,
+  SiGithub,
+} from "react-icons/si";
 
 export const technologiesData = [
   {
     name: "Ansible",
-    icon: <Ansible width={50} />,
+    icon: <SiAnsible width={50} />,
   },
   {
     name: "Terraform",
-    icon: <Terraform width={50} />,
+    icon: <SiTerraform width={50} />,
   },
   {
     name: "Docker",
-    icon: <Docker width={50} />,
+    icon: <SiDocker width={50} />,
   },
   {
     name: "Git",
-    icon: <Git width={50} />,
+    icon: <SiGit width={50} />,
   },
   {
     name: "Github",
-    icon: <Github width={50} />,
+    icon: <SiGithub width={50} />,
   },
   {
     name: "Shell",
-    icon: <Shell width={50} />,
+    icon: <SiGnubash width={50} />,
   },
   {
     name: "Python",
-    icon: <Python width={50} />,
+    icon: <SiPython width={50} />,
   },
   {
     name: "C++",
-    icon: <Cplus width={50} />,
-  },
-  {
-    name: "Java",
-    icon: <Java width={50} />,
+    icon: <SiCplusplus width={50} />,
   },
 ];
