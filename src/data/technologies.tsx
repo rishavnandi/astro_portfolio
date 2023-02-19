66a83e661f53df6680850171fa3d8147c60dedc6
Rishav Nandi <rishavnandi2@gmail.com>
2023-02-20 00:50:29 +0530

    Added colors to tech stack icons

--- src/data/technologies.tsx
+++ src/data/technologies.tsx
@@ -12,34 +12,34 @@ import {
 export const technologiesData = [
   {
     name: "Ansible",
-    icon: <SiAnsible size={50} />,
+    icon: <SiAnsible size={50} style={{ color: "#ff5750" }} />,
   },
   {
     name: "Terraform",
-    icon: <SiTerraform size={50} />,
+    icon: <SiTerraform size={50} style={{ color: "#7b42bc" }} />,
   },
   {
     name: "Docker",
-    icon: <SiDocker size={50} />,
+    icon: <SiDocker size={50} style={{ color: "#2496ed" }} />,
   },
   {
     name: "Git",
-    icon: <SiGit size={50} />,
+    icon: <SiGit size={50} style={{ color: "#f05030" }} />,
   },
   {
     name: "Github",
-    icon: <SiGithub size={50} />,
+    icon: <SiGithub size={50} style={{ color: "#ff5750" }} />,
   },
   {
     name: "Shell",
-    icon: <SiGnubash size={50} />,
+    icon: <SiGnubash size={50} style={{ color: "#ff5750" }} />,
   },
   {
     name: "Python",
-    icon: <SiPython size={50} />,
+    icon: <SiPython size={50} style={{ color: "#fed140" }} />,
   },
   {
     name: "C++",
-    icon: <SiCplusplus size={50} />,
+    icon: <SiCplusplus size={50} style={{ color: "#00549e" }} />,
   },
 ];
